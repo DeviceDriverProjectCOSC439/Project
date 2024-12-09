f41def750ba5b619d5ac2ec498c2ae4267414bd2
Joaquin Villagomez <villagomezjoaquin6@gmail.com>
2024-12-09 17:39:10 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Directory to monitor
-MONITOR="/mnt"
-
-# Your compiled C program to trigger
+MONITOR="/media/joaquin/usb"
 ENCRYPT="/home/joaquin/Documents/final/SimpleLinuxDriver/encrypt"
+DECRYPT="/home/joaquin/Documents/final/SimpleLinuxDriver/decrypt"
+INOTIFYWAIT="/usr/bin/inotifywait"
 
-# Use inotifywait to monitor for 'create' and 'modify' events
-inotifywait -m "$MONITOR" -e create,modify |
+$INOTIFYWAIT -m "$MONITOR" -e create -e moved_from |
 while read -r directory event filename; do
     echo "File '$filename' was $event in $directory"
-    # Trigger your C program and pass the full file path as an argument
-    "$ENCRYPT" "$directory$filename"
+    
+    full_file_path="$directory$filename"
+
+    if [[ $event == "CREATE" ]]; then
+        bash -c "$ENCRYPT $full_file_path; exec bash"
+    fi
+
+    if [[ $event == "MOVED_FROM" ]]; then
+        bash -c "$DECRYPT $full_file_path; exec bash"
+    fi
 done
