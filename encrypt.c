b75b8c05936a78d557971f3a2e15cf5b93e91de4
Joaquin Villagomez <villagomezjoaquin6@gmail.com>
2024-12-09 17:37:22 +0000

    Update encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -7,16 +7,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Get the file path from the command-line argument
     char *file_path = argv[1];
 
-    
     char encrypted_file[512];
     snprintf(encrypted_file, sizeof(encrypted_file), "%s.enc", file_path);  
    
     char openssl_command[1024];
     snprintf(openssl_command, sizeof(openssl_command),
-             "openssl aes-256-cbc -salt -a -e -in \"%s\" -out \"%s\"",
+             "openssl pkeyutl -encrypt -pubin -inkey /home/joaquin/Documents/final/SimpleLinuxDriver/public.key -in \"%s\" -out \"%s\"",
              file_path, encrypted_file);
 
     int ret = system(openssl_command);
